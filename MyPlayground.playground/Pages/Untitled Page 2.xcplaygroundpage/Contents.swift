fa364bb8f390c116b23f936fb0de6c2c5e346e58
San Nimona <kaybryanmc@gmail.com>
2024-04-22 21:13:53 +0300

    Auto layout

--- MyPlayground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
 //: [Previous](@previous)
 
-import Foundation
+func PrintAndCount(_ string: String) -> Int {
+    print(string)
+    let num = string.count
+    return num
+}
+func PrintWithoutCount(_ string: String){
+    let _ = PrintAndCount(string)
+}
+PrintAndCount("hey, brian")
+PrintWithoutCount("hey, brian")
 
-var greeting = "Hello, playground"
+func minMax(array: [Int]) -> (min: Int, max: Int)?{
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for num in array[1..<array.count]{
+        if num > currentMax{
+            currentMax = num
+        }
+        if num < currentMin{
+            currentMin = num
+        }
+    }
+    return (currentMin, currentMax)
+}
+if let bounds = minMax(array: [2, 3, 54, 23, 12, 19, 3, 1, 0, 2, 32]){
+    print(bounds)
+}
 
 //: [Next](@next)
