3ba24fa3aecc10a417d0565cae62ae28b28b9903
San Nimona <kaybryanmc@gmail.com>
2024-05-29 16:06:48 +0300

    hi

--- AutoLayout_code/CrossView/CrossView/CrossView/ViewController.swift
+++ AutoLayout_code/CrossView/CrossView/CrossView/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var viewB: UIView!
+    
+    @IBOutlet weak var centerConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewB.removeConstraint(centerConstraint)
+        
+        myButton.centerXAnchor.constraint(equalTo: viewB.centerXAnchor).isActive=true
+        myLabel.centerXAnchor.constraint(equalTo: viewB.centerXAnchor).isActive=true
+        myLabel.trailingAnchor.constraint(equalTo: myButton.trailingAnchor).isActive=true
+        createLayout()
         // Do any additional setup after loading the view.
     }
-
-
+    func createLayout() {
+        let superview = self.view
+        
+        let myLabel = UILabel()
+        myLabel.translatesAutoresizingMaskIntoConstraints = true
+        myLabel.text = "My Label"
+    }
 }
 
